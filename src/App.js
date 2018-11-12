a4c6b8c97ce39aaa457ae9b59ec2ae156601d3ca
Kenny Crump <kennycrump@Kennys-MacBook-Pro.local>
2018-11-12 13:11:50 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Nav from './Components/Nav'
 import './App.css';
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrapper>
+        <Nav />
+      </AppWrapper>
     );
   }
 }
 
 export default App;
+
+
+const AppWrapper = styled.div`
+  text-align: left;
+`
+const AppHeader = styled.header`
+  background-color: #233455;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: blue;
+`
